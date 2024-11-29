c4acd89d6e7e2d29610691bd342d1e84f019f09c
Rakib Hasan <codermrhasan@gmail.com>
2024-11-29 18:22:13 +0600

    add billing history

---
+++ README.md
@@ -0,0 +1,12 @@
+# Twilio SMS Portal
+
+This is a fully working Twilio SMS Portal. With real time messaging.
+
+## Preview
+![SMS Portal Overview](docs/media/preview1.png)
+
+## Built With
+- HTML, CSS, JavaScript, jQuery
+- Django
+- Django Channels
+
---
+++ apps/portal/views/billing.py
@@ -0,0 +1,65 @@
+from django.views import View
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import JsonResponse
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from apps.portal.models import TwilioAccount, PhoneNumber, Contact, Conversation, Message
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core import serializers
+from django.db.models import Count, Q, Value, F, ExpressionWrapper, FloatField
+from django.db.models.functions import Coalesce
+from django.utils.timezone import make_aware
+from datetime import datetime, timedelta
+
+
+class Billing(LoginRequiredMixin, View):
+    def get(self, request):
+
+        # today 
+        today = datetime.today().date()
+
+        data = request.GET
+
+        startdate = data.get('startdate') if data.get('startdate') else today - timedelta(days=30)
+        enddate = data.get('enddate') if data.get('enddate') else today
+        
+        # Define date range
+        start_date = startdate
+        end_date = enddate
+
+        # Aggregate incoming and outgoing message counts per user
+        history_data = TwilioAccount.objects.filter(
+            user__isnull=False  # Ensures TwilioAccount is related to a user
+        ).annotate(
+            username=F('user__username'),
+            total_incoming=Count(
+                'conversations__messages',
+                filter=Q(
+                    conversations__messages__sender=Message.SENDER.CUSTOMER,
+                    conversations__messages__created_at__range=(start_date, end_date)
+                )
+            ),
+            total_outgoing=Count(
+                'conversations__messages',
+                filter=Q(
+                    conversations__messages__sender=Message.SENDER.ME,
+                    conversations__messages__created_at__range=(start_date, end_date)
+                )
+            ),
+            total_messages=ExpressionWrapper(
+                Count(
+                    'conversations__messages',
+                    filter=Q(
+                        conversations__messages__created_at__range=(start_date, end_date)
+                    )
+                ),
+                output_field=FloatField()
+            )
+        ).values('username', 'total_incoming', 'total_outgoing', 'total_messages')
+
+        context = {
+            'history': history_data,
+            'startdate': startdate,
+            'enddate': enddate
+        }
+
+        return render(request, 'portal/billing.html', context=context)
